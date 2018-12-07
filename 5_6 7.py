442282e88f67e81e5f3f9d61c84ddcf39df856c9
zpszu <45482003+zpszu@users.noreply.github.com>
2018-12-07 19:29:03 +0800

    chapter 5

---
+++ 5_6 7.py
@@ -0,0 +1,32 @@
+#
+age = 5
+
+if age < 2:
+    print("He is baby.")
+elif age < 4:
+    print("He is learning walking.")
+elif age < 13:
+    print("He is a child.")
+elif age < 20:
+    print("He is a teenager.")
+elif age < 65:
+    print("He is an adult.")
+elif age >= 65:
+    print("He is an elder man")
+
+#
+if 'pear' in favorite_fruits:
+    print("You really like pear")
+if 'watermelon' in favorite_fruits:
+    print("You really like watermelon")
+if 'banana' in favorite_fruits:
+    print("You really like banana")
+
+for fruit in fruits:
+    if fruit in favorite_fruits:
+        print("You really like " + fruit)
+
+
+
+
+
